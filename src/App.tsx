3ac3040a5e17124f4b8f399671a8ac180ae76548
Jindapxnn <jindapol.koh@gmail.com>
2025-05-08 20:52:17 +0700

    Add Main Web

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App(){
+  return(
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-[50px]">Hello LnwGum</p>
+      </div>
+    </>
+  )
+}
